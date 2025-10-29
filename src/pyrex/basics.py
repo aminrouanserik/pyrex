3450f5e038831f6db72ef02f4a0132b7fee44495
Amin Rouan Serik <aminrouanserik@gmail.com>
2025-10-29 16:45:00 +0100

    Analysis complete
    Added missing docstrings and completed the characterisation and analysis of current Pyrex behaviors. This will form a testbed for improvements currently considered to be added to this framework

--- src/pyrex/basics.py
+++ src/pyrex/basics.py
@@ -5,32 +5,47 @@ from scipy import interpolate
 import statistics
 
 
-def read_pkl(file_dir):
+def read_pkl(file_dir: str) -> any:
+    """Returns data from a specified pickle file.
 
+    Args:
+        file_dir (str): Directory to read data from.
+
+    Returns:
+        any: Data in the file.
+    """
     with open(file_dir, "rb") as f:
         data = pickle.load(f)
     return data
 
 
-def write_pkl(outfname, data_dict):
+def write_pkl(outfname: str, data_dict: dict) -> None:
+    """Writes a dictionary to a pickle file in a specified directory.
 
+    Args:
+        outfname (str): Path to the file to write to.
+        data_dict (dict): Dictionary to write to the file.
+    """
     f = open(outfname, "wb")
     pickle.dump(data_dict, f)
     f.close()
 
 
-def checkIfDuplicates(listofElems):
-
-    for elem in listofElems:
-        if listofElems.count(elem) > 1:
-            return True
-    return False
-
-
 def get_filename(
-    dirfile="/home/amin/Projects/School/Masters/25_26-Thesis/pyrex/data/",
-):
+    dirfile: str = "/home/amin/Projects/School/Masters/25_26-Thesis/pyrex/data/",
+) -> str:
+    """Get the filename of the pickle data file.
 
+    Args:
+        dirfile (str, optional): Directory in which pickle file is stored. Defaults to "/home/amin/Projects/School/Masters/25_26-Thesis/pyrex/data/".
+
+    Raises:
+        ValueError: Raised if there are no pickle files in the directory,
+        ValueError: Raised if more than one pickle file was found.
+
+    Returns:
+        str: Path to the pickle file.
+    """
     r = 0
     os.chdir(dirfile)
 
@@ -56,19 +71,41 @@ def get_filename(
     return dfs
 
 
-def interp1D(trainkey, trainval, testkey):
+def interp1D(
+    trainkey: list[float], trainval: list[float], testkey: list[float]
+) -> list[float]:
+    """Interpolates a key between various points in the training data.
 
+    Args:
+        trainkey (list[float]): List of values in a grid on which the interpolation is based.
+        trainval (list[float]): Values corresponding to the train grid.
+        testkey (list[float]): List of values in a grid of which to get the interpolant.
+
+    Returns:
+        list[float]: The interpolateed data.
+    """
     newkey, newval = check_duplicate_training(trainkey, trainval)
 
     if testkey < min(trainkey) or testkey > max(trainkey):
         interp = interpolate.interp1d(newkey, newval, fill_value="extrapolate")
     else:
-        interp = interpolate.interp1d(newkey, newval)
+        interp = interpolate.interp1d(trainkey, trainval)
     return interp(testkey)
 
 
-def check_duplicate_training(trainkey, trainval):
+def check_duplicate_training(
+    trainkey: list[float], trainval: list[float]
+) -> tuple[list[float], list[float]]:
+    """Checks whether there is any duplicate keys and calculates the median.
 
+    Args:
+        trainkey (list[float]): List of values in a grid on which the interpolation is based.
+        trainval (list[float]): Values corresponding to the train grid.
+
+    Returns:
+        tuple[list[float], list[float]]: Tuple of the new list of values in a grid on which the interpolation is based and
+        the values corresponding to that grid.
+    """
     d = {}
     newkey = []
     newval = []
@@ -80,13 +117,3 @@ def check_duplicate_training(trainkey, trainval):
         newkey.append(key)
         newval.append(statistics.median(d[key]))
     return newkey, newval
-
-
-__all__ = [
-    "read_pkl",
-    "write_pkl",
-    "checkIfDuplicates",
-    "checkIfFilesExist",
-    "interp1D",
-    "check_duplicate_training",
-]
--- src/pyrex/tools.py
+++ src/pyrex/tools.py
@@ -3,36 +3,103 @@ from scipy.optimize import curve_fit
 from scipy.interpolate import make_interp_spline
 
 
-def interp_omega(time_circular, time_eccentric, omega_circular):
+def interp_omega(
+    time_circular: np.ndarray, time_eccentric: np.ndarray, omega_circular: np.ndarray
+) -> np.ndarray:
+    """Interpolates omega and casts to a new timegrid using a spline.
 
+    Args:
+        time_circular (np.ndarray): The old time grid.
+        time_eccentric (np.ndarray): The new time grid.
+        omega_circular (np.ndarray): The values of omega with which to create the spline.
+
+    Returns:
+        np.ndarray: The omega cast to a new time grid.
+    """
     interpol = make_interp_spline(time_circular, omega_circular)
     omega_interp = interpol(time_eccentric)
     return omega_interp
 
 
-def f_sin(xdata, amplitude, B, freq, phase):
+def f_sin(
+    xdata: np.ndarray, amplitude: float, B: float, freq: np.ndarray, phase: float
+) -> np.ndarray:
+    """The fitting function of the eccentricity caused modulations to the amplitude or omega.
 
+    Args:
+        xdata (np.ndarray): The circularized amplitude or omega to which the power law has been applied.
+        amplitude (float): The amplitude parameter A of the fitting function.
+        B (float): The factor in the exponent of the fitting function.
+        freq (np.ndarray): The frequency of the waveform.
+        phase (float): The free fitting parameter phi.
+
+    Returns:
+        np.ndarray: Eccentricity caused modulations to the amplitude or omega.
+    """
     sin_func = (
         amplitude * np.exp(B * xdata) * np.sin(xdata * freq / (2 * np.pi) + phase)
     )
     return sin_func
 
 
-def fit_sin(xdata, ydata):
+def fit_sin(xdata: np.ndarray, ydata: np.ndarray) -> tuple[np.ndarray, np.ndarray]:
+    """Performs the fit of eccentricity caused modulations to the amplitude or omega.
 
+    Args:
+        xdata (np.ndarray): The circularized amplitude or omega to which the power law has been applied.
+        ydata (np.ndarray): The eccentricity caused amplitude or omega.
+
+    Returns:
+        tuple[np.ndarray, np.ndarray]: Optimal values for the parameter and its evaluation.
+    """
     popt, _ = curve_fit(f_sin, xdata, ydata)
     fit_result = f_sin(xdata, *popt)
     return popt, fit_result
 
 
-def calculate_x(old_time, omega, new_time):
+def calculate_x(
+    old_time: np.ndarray, omega: np.ndarray, new_time: np.ndarray
+) -> np.ndarray:
+    """Calculates the geometric frequency x. Will create a spline on which new values for x are computed.
 
+    Args:
+        old_time (np.ndarray): The original time grid.
+        omega (np.ndarray): The values for omega on the original time grid.
+        new_time (np.ndarray): The new time grid.
+
+    Returns:
+        np.ndarray: The geometric frequency on the `new_time` timegrid.
+    """
     interp_omega = make_interp_spline(old_time, omega)
     x = interp_omega(new_time[0]) ** (2 / 3)
     return x
 
 
-def get_noncirc_params(somedict):
+def get_noncirc_params(
+    somedict: dict,
+) -> tuple[list[float], list[float], list[float], list[list[any]], list[list[any]]]:
+    """Extracts and organizes non-circular waveform fitting parameters from a results dictionary.
+
+    Args:
+        somedict (dict): Dictionary containing fitted parameter results. Must include the following keys:
+            - "q" (float): Mass ratio.
+            - "e_ref" (float): Reference eccentricity.
+            - "x" (float): Dimensionless post-Newtonian parameter or time reference.
+            - "A_omega", "B_omega", "freq_omega", "phi_omega" (float or list[float]):
+              Frequency fit parameters.
+            - "A_amp", "B_amp", "freq_amp", "phi_amp" (float or list[float]):
+              Amplitude fit parameters.
+
+    Returns:
+        tuple[list[float], list[float], list[float], list[list[any]], list[list[any]]]: A tuple containing:
+            - ecc_q (list[float]): Mass ratio values.
+            - ecc_e (list[float]): Reference eccentricity values.
+            - ecc_x (list[float]): PN or time parameter values.
+            - par_omega (list[list[any]]): Lists of frequency-related fit parameters
+              [A_omega, B_omega, freq_omega, phi_omega].
+            - par_amp (list[list[any]]): Lists of amplitude-related fit parameters
+              [A_amp, B_amp, freq_amp, phi_amp].
+    """
 
     ecc_q = somedict["q"]
     ecc_e = somedict["e_ref"]
@@ -49,12 +116,3 @@ def get_noncirc_params(somedict):
     par_omega = [ecc_A_omega, ecc_B_omega, ecc_freq_omega, ecc_phi_omega]
     par_amp = [ecc_A_amp, ecc_B_amp, ecc_freq_amp, ecc_phi_amp]
     return ecc_q, ecc_e, ecc_x, par_omega, par_amp
-
-
-__all__ = [
-    "interp_omega",
-    "f_sin",
-    "fit_sin",
-    "calculate_x",
-    "get_noncirc_params",
-]
