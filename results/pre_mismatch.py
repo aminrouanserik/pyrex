Added figures and fixed minus sign conventions

results/pre_mismatch.py
@@ -91,12 +91,12 @@ plt.imshow(
     mismatches,
     origin="lower",
     aspect="auto",
-    extent=[
+    extent=(
         total_masses.min(),
         total_masses.max(),
         eccentricities.min(),
         eccentricities.max(),
-    ],
+    ),
     cmap="viridis",
     norm=colors.LogNorm(
         vmin=np.nanmin(mismatches[mismatches > 0]),  # avoid log(0)

results/strain_zoomedin.py
@@ -1,25 +1,22 @@
 import matplotlib.patches as patches
 import matplotlib.pyplot as plt
-import numpy as np
-from qcextender import units
 from qcextender.waveform import Waveform
 
 from pyrex.eccentric_extension import generate_eccentric_waveform
 
-# --- your data prep ---
 kwargs = {
     "mass1": 15,
     "mass2": 15,
     "inclination": 0,
     "coa_phase": 0,
-    "delta_t": 1.0 / 5000,
-    "f_lower": 20,
-    "f_ref": (0.075 / 50) ** (3 / 2) * 2 * np.pi,
-    "distance": 10,
+    "delta_t": 1.0 / 4096,
+    "f_lower": 10,
+    "f_ref": 20,
+    "distance": 100,
+    "eccentricity": 0.1,
 }
 
 phen = Waveform.from_model("IMRPhenomTE", [(2, 2)], **kwargs)
-kwargs.update({"eccentricity": 0.1})
 phen_ecc = generate_eccentric_waveform(
     "IMRPhenomTE",
     [(2, 2)],
@@ -28,21 +25,20 @@ phen_ecc = generate_eccentric_waveform(
     **kwargs,
 )
 
-zoom_t_min = phen.time[12443]
+zoom_t_min = phen.time[10000]
 zoom_t_max = phen.time[-1]
-zoom_y_min = min(phen_ecc[2, 2][12443:]) * 1.2
-zoom_y_max = max(phen_ecc[2, 2][12443:]) * 1.2
+zoom_y_min = min(phen_ecc[2, 2][10000:]) * 1.2
+zoom_y_max = max(phen_ecc[2, 2][10000:]) * 1.2
 
-fig, axes = plt.subplots(1, 2, figsize=(8, 5), width_ratios=[0.7, 0.3])
+fig, axes = plt.subplots(1, 2, figsize=(10, 5), width_ratios=[0.65, 0.35])
 
 ax_full = axes[0]
 ax_full.plot(phen_ecc.time, phen_ecc[2, 2], label="IMRPhenomTPyrex")
-ax_full.plot(phen.time, phen[2, 2], label="IMRPhenomD")
+ax_full.plot(phen.time, phen[2, 2], label="IMRPhenomTE")
 
 ax_full.set_title("Full Waveform", fontsize=14)
 ax_full.set_ylabel("Strain (m)")
 ax_full.set_xlabel("Time (s)")
-ax_full.legend(frameon=False)
 
 rect = patches.Rectangle(
     (zoom_t_min, zoom_y_min),
@@ -55,22 +51,15 @@ rect = patches.Rectangle(
 )
 ax_full.add_patch(rect)
 
-
 ax_zoom = axes[1]
-ax_zoom.plot(phen_ecc.time, phen_ecc[2, 2], label="IMRPhenomDPyrex")
-ax_zoom.plot(phen.time, phen[2, 2], label="IMRPhenomD")
+ax_zoom.plot(phen_ecc.time, phen_ecc[2, 2], label="IMRPhenomTPyrex")
+ax_zoom.plot(phen.time, phen[2, 2], label="IMRPhenomTE")
 
 ax_zoom.set_xlim(zoom_t_min, zoom_t_max)
 ax_zoom.set_ylim(zoom_y_min, zoom_y_max)
-ax_zoom.set_title("Zoomed Region", fontsize=14)
-# ax_zoom.set_ylabel("Strain (m)")
+ax_zoom.set_title("Around merger", fontsize=14)
 ax_zoom.set_xlabel("Time (s)")
-ax_zoom.legend(frameon=False)
-
-# conns = [
-#     ((zoom_t_min, zoom_y_max), (zoom_t_min, zoom_y_max)),
-#     ((zoom_t_max, zoom_y_max), (zoom_t_max, zoom_y_max)),
-# ]
+ax_zoom.legend(loc="upper right")
 
 plt.tight_layout()
 plt.show()

src/pyrex/eccentric_fit.py
@@ -214,7 +214,7 @@ def construct_lookup(
     circ_lookup = {}
     for c in circ_waves:
         time = c.time
-        mask = (time > (time[0] + 250)) & (time <= -29)
+        mask = (time >= -1500) & (time <= -29)
         t = time[mask]
         omega = c.omega()[mask]
         amp = c.amp()[mask]
@@ -257,7 +257,7 @@ def get_e_X(
     Returns:
         NDArray[np.floating]: The eccentric contribution to X, the amplitude or instantaneous frequency.
     """
-    mask = (wave.time > (wave.time[0] + 250)) & (wave.time <= -29)
+    mask = (wave.time >= -1500) & (wave.time <= -29)
     ecc_interp = make_interp_spline(wave.time[mask], get_component(wave)[mask])
     ecc_vals = ecc_interp(new_time)
 